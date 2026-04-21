e9f71e4e7d2beb2783efebd55ec92bcddd6c654e
Olamgir2002 <olamgir202@gmail.com>
2026-04-21 19:02:38 +0500

    feat: add quiz schema

--- src/database/schema.ts
+++ src/database/schema.ts
@@ -1,5 +1,14 @@
 import { relations } from 'drizzle-orm'
-import { pgTable, text, timestamp, uuid } from 'drizzle-orm/pg-core'
+import {
+  jsonb,
+  pgTable,
+  text,
+  timestamp,
+  uuid,
+  boolean,
+  integer,
+  pgEnum,
+} from 'drizzle-orm/pg-core'
 
 export const users = pgTable('users', {
   id: uuid('id').defaultRandom().primaryKey(),
@@ -85,3 +94,84 @@ export const userApiKeysRelations = relations(userApiKeys, ({ one }) => ({
     references: [users.id],
   }),
 }))
+
+/*--- Quizzez ---*/
+
+export const questionTypeEnum = pgEnum('question_type', [
+  'single_choice',
+  'multiple_choice',
+  'text',
+  'true_false',
+])
+
+export const quizzes = pgTable('quizzes', {
+  id: uuid('id').primaryKey().defaultRandom().notNull(),
+
+  title: text('title').notNull(),
+
+  userId: uuid('user_id')
+    .notNull()
+    .references(() => users.id),
+
+  type: questionTypeEnum('type'),
+
+  properties: jsonb('properties').notNull(),
+
+  isTimerEnabled: boolean('is_timer_enabled').notNull().default(false),
+
+  timerDuration: integer('timer_duration'),
+
+  completeBy: timestamp('complete_by', { mode: 'date' }),
+
+  userInstructions: text('user_instructions'),
+
+  completedAt: timestamp('completed_at', { mode: 'date' }),
+
+  createdAt: timestamp('created_at', { mode: 'date' }).notNull().defaultNow(),
+
+  uploadedAt: timestamp('uploaded_at', { mode: 'date' }),
+})
+
+export const questions = pgTable('questions', {
+  id: uuid('id').primaryKey().defaultRandom().notNull(),
+
+  quizId: uuid('quiz_id')
+    .notNull()
+    .references(() => quizzes.id, { onDelete: 'cascade' }),
+
+  text: text('text').notNull(),
+
+  type: questionTypeEnum('type').notNull(),
+  //Don't miss this one guys(ladies ?_?), it is for question's position in the list
+  position: integer('position').notNull(),
+
+  createdAt: timestamp('created_at', { mode: 'date' }).defaultNow().notNull(),
+
+  updatedAt: timestamp('updated_at', { mode: 'date' })
+    .defaultNow()
+    .notNull()
+    .$onUpdate(() => new Date()),
+})
+
+export const questionOptions = pgTable('question_options', {
+  id: uuid('id').primaryKey().defaultRandom().notNull(),
+
+  questionId: uuid('question_id')
+    .notNull()
+    .references(() => questions.id, { onDelete: 'cascade' }),
+
+  text: text('text').notNull(),
+
+  explanation: text('explanation'),
+
+  isCorrect: boolean('is_correct').notNull().default(false),
+
+  position: integer('position').notNull(),
+
+  createdAt: timestamp('created_at', { mode: 'date' }).defaultNow().notNull(),
+
+  updatedAt: timestamp('updated_at', { mode: 'date' })
+    .defaultNow()
+    .notNull()
+    .$onUpdate(() => new Date()),
+})
