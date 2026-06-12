a27cc257fec9682906d9c39ff354011167db7de1
Javohir Mirzakhalov <javohirmirzakhalov@gmail.com>
2026-06-12 16:55:07 +0500

    refactor: use winston logger in server startup

--- src/server.ts
+++ src/server.ts
@@ -3,11 +3,12 @@ import dotenv from 'dotenv'
 dotenv.config()
 
 import app from './app'
+import { logger } from './config/logger'
 import { startFeedbackCron } from './crons/feedbackCron'
 
 const PORT = process.env.PORT || 3000
 
 app.listen(PORT, () => {
-  console.log(`Server running on http://localhost:${PORT}`)
+  logger.info(`Server running on http://localhost:${PORT}`)
   startFeedbackCron()
 })
